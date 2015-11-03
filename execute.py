ce01c641894c8b7eb213085b2311508c899f6208
greeneth <green.ethan@gmail.com>
2015-11-03 15:46:15 -0800

    Added execution file to automatically execute the scripts

---
+++ execute.py
@@ -0,0 +1,10 @@
+#Executes automated SQL queries
+
+execfile('Clearwater_CH1.py')
+execfile('Clearwater_STH.py')
+
+execfile('Lower_Snake_CH1.py')
+execfile('Lower_Snake_STH.py')
+
+execfile('Salmon_CH1.py')
+execfile('Salmon_STH.py')
