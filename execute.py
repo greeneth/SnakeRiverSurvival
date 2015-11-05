cd3bf6e354777dedc9ebcdbb758d7f0c4d5f69a9
greeneth <green.ethan@gmail.com>
2015-11-05 10:04:08 -0800

    Final versions of the code and a python script to execute all of them automatically

--- execute.py
+++ execute.py
@@ -3,8 +3,8 @@
 execfile('Clearwater_CH1.py')
 execfile('Clearwater_STH.py')
 
-execfile('Lower_Snake_CH1.py')
-execfile('Lower_Snake_STH.py')
+execfile('Snake_CH1.py')
+execfile('Snake_STH.py')
 
 execfile('Salmon_CH1.py')
 execfile('Salmon_STH.py')
